a2cba246d7e8c8f3d85958e07052979ce3d3a4e8
marcelotoi <marcelo_toi@hotmail.com>
2018-03-05 14:35:03 -0300

    small fix

--- dev/workbench.js
+++ dev/workbench.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../src/style/_index";
 
-<<<<<<< HEAD
 import { Button } from "../src/index";
-=======
-import { Example } from "../src/index";
->>>>>>> d05ffc586b82294f09c897945e9f4986952326bd
 
 class Workbench extends React.Component {
 
@@ -19,18 +15,10 @@ class Workbench extends React.Component {
     render() {
         return (
             <section className="test-area">
-<<<<<<< HEAD
                 <Button label="Exemplo" onClick={()=>console.log("Click")}/>
-=======
-                <Example label="Exemplo" />
->>>>>>> d05ffc586b82294f09c897945e9f4986952326bd
             </section>
         )
     }
 }
 
 ReactDOM.render(<Workbench />, document.querySelector("#app-container"));
-<<<<<<< HEAD
-=======
-
->>>>>>> d05ffc586b82294f09c897945e9f4986952326bd
