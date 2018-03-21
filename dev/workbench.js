4270d4e86e100914a64deb537b009f5748de0485
Thiago Loddi <thiago@thevelops.com>
2018-03-21 18:43:27 -0300

    [tvm26] fixed conflicts

--- dev/workbench.js
+++ dev/workbench.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../src/style/_index";
 
-<<<<<<< HEAD
-import { FloatingActionButton } from "../src/index";
-=======
 import { Button } from "../src/index";
->>>>>>> 074ca17bda29e2972688675373ace7b1be373eb8
 
 class Workbench extends React.Component {
 
@@ -19,11 +15,7 @@ class Workbench extends React.Component {
     render() {
         return (
             <section className="test-area">
-<<<<<<< HEAD
-                <FloatingActionButton icon="https://storage.googleapis.com/material-icons/external-assets/v4/icons/svg/ic_call_white_24px.svg" fixed/>
-=======
                 <Button label="Exemplo" onClick={()=>console.log("Click")}/>
->>>>>>> 074ca17bda29e2972688675373ace7b1be373eb8
             </section>
         )
     }
